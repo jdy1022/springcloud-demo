b84a55e501f58fbdb5f6811bc5aa73c04fbe200f
jdy1022@126.com <jdy1022@126.com>
2020-05-31 21:43:51 +0800

    EurekaMain7001和Eureka7002集群的搭建

--- cloud-eureka-server7002/src/main/java/com/jdy/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/jdy/springcloud/EurekaMain7002.java
@@ -2,16 +2,16 @@ package com.jdy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Mr.jdy
- * @create 2020-05-29 20:27
+ * @create 2020-05-31 21:27
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class OrderMain80 {
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
