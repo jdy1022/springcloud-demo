cc0d6c5dc60a38dde1b8e137e873f38d4e534662
jdy1022@126.com <jdy1022@126.com>
2020-06-01 21:38:58 +0800

    服务提供者注册到zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/jdy/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.jdy.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author Mr.jdy
+ * @create 2020-06-01 20:25
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+    @RequestMapping("/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
