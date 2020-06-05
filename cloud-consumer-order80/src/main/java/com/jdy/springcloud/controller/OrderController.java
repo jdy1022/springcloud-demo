04aadf2b9c24992b9a40b816a591d439f22c327f
jdy1022@126.com <jdy1022@126.com>
2020-06-05 20:30:24 +0800

    RestTemplate 的forEntity

--- cloud-consumer-order80/src/main/java/com/jdy/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/jdy/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.jdy.springcloud.controller;
 import com.jdy.springcloud.entities.CommonResult;
 import com.jdy.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +26,28 @@ public class OrderController {
     public CommonResult<Payment> create(Payment payment){
         return template.postForObject(PAYMENT_URL+"/payment/create",payment, CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment1/create")
+    public CommonResult<Payment> create2(Payment payment){
+        ResponseEntity<CommonResult> commonResultResponseEntity = template.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        return commonResultResponseEntity.getBody();
+    }
+
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return template.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> forEntity = template.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        CommonResult result =null;
+        if(forEntity.getStatusCode().is2xxSuccessful()){
+            result = forEntity.getBody();
+        }else{
+            result = new CommonResult();
+        }
+        return result;
+    }
+
 }
