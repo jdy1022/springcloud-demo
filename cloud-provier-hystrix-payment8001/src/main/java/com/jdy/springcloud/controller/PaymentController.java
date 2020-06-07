4345193cf6d4f324a5972a48615ae7ff0fb712bb
jdy1022@126.com <jdy1022@126.com>
2020-06-07 23:43:46 +0800

    Hystrix熔断

--- cloud-provier-hystrix-payment8001/src/main/java/com/jdy/springcloud/controller/PaymentController.java
+++ cloud-provier-hystrix-payment8001/src/main/java/com/jdy/springcloud/controller/PaymentController.java
@@ -40,12 +40,12 @@ public class PaymentController {
         return result;
     }
 
-    /*//====服务熔断
+    //服务熔断
     @GetMapping("/payment/circuit/{id}")
     public String paymentCircuitBreaker(@PathVariable("id") Integer id)
     {
         String result = paymentService.paymentCircuitBreaker(id);
-        log.info("****result: "+result);
+        System.out.println("****result: "+result);
         return result;
-    }*/
+    }
 }
